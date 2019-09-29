d957a19f4cdb9614fa704fb2cd1a2c39928a3966
Simen Sverdrup-Thygeson <simenthygeson@gmail.com>
2019-09-29 18:57:10 +0200

    Added conversion functionality and tried to implement HTTP get() and post()-functionality

--- ConversionServlet/src/conversionservlet/ConversionServlet.java
+++ ConversionServlet/src/conversionservlet/ConversionServlet.java
@@ -1,19 +1,71 @@
 package conversionservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
 import java.io.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import org.osgi.util.measurement.*;
 
-
+@WebServlet("/ConversionServlet") //http://localhost:8080/ConversionServlet
 public class ConversionServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+	
+	protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+ 
+		// read form fields
+        double value = Float.parseFloat(request.getParameter("value"));
+        String from_unit = request.getParameter("from_unit");
+        String to_unit = request.getParameter("to_unit");
+ 
+        // calculate new value
+        UnitConverter conv = new UnitConverter(from_unit, to_unit, value);
+        double newValue = conv.convert();
+        
+        // get response writer
+        PrintWriter writer = response.getWriter();
+        
+    	String htmlResponse = "";
+
+        if (newValue == -1) {
+        	htmlResponse = "<html>";
+            htmlResponse += "<h2>" + "Unknown unit, cannot convert!" + "</h2>";
+            htmlResponse += "</html>";
+
+        }
+        else {
+            // build HTML code
+        	htmlResponse = "<html>";
+            htmlResponse += "<h2>" + value + " " + from_unit +" is " + newValue + to_unit + "</h2>";   
+            htmlResponse += "</html>";
+        }
+         
+        // return response
+        writer.println(htmlResponse);
+ 
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-				resp.setContentType("text/plain");
-				resp.getWriter().write("Input value: ");
-				resp.getWriter().write("\nInput unit: ");
-				resp.getWriter().write("\nInput wanted unit: ");
+		
+        // get response writer
+        PrintWriter writer = response.getWriter();
+        
+        // build HTML code
+		String htmlResponse = "<html>";
+		htmlResponse +=	"<form name=\"conversionForm\" method=\"post\" action=\"conversionServlet\">"; 
+		htmlResponse +=	" Value: <input type=\"number\" name=\"value\"/>"; 
+		htmlResponse +=	" From unit: <input type=\"text\" name=\"from_unit\"/>";
+		htmlResponse +=	" To unit: <input type=\"text\" name=\"to_unit\"/>"; 
+		htmlResponse +=	" <input type=\"submit\" value=\"Convert\" /></form>";
+		htmlResponse +=	"</html>";
+         
+        // return response
+        writer.println(htmlResponse);
 	}
 	
 	double Converter(double value, Unit from_unit, Unit to_unit) {
--- ConversionServlet/src/conversionservlet/UnitConverter.java
+++ ConversionServlet/src/conversionservlet/UnitConverter.java
@@ -2,83 +2,86 @@ package conversionservlet;
 
 public class UnitConverter { //values are x/1meter
 	String fromUnit, toUnit;
-	static double INCHES = 0.0254001;
 	static double FEET = 0.3048;
 	static double MILES = 1609.35;
-	static double MILLIMETERS = 0.001;
-	static double CENTIMETERS = 0.01;
-	static double METERS = 1;
+	static double INCHES = 0.0254;
 	static double KILOMETERS = 1000;
-	private double val, meters, converted;
+	static double METERS = 1;
+	static double CENTIMETERS = 0.01;
+	static double MILLIMETERS = 0.001;
+	private double value, converted;
  
-	public UnitConverter(String fromUnit, String toUnit) {
-		this.fromUnit = fromUnit;
-		this.toUnit = toUnit;
+	public UnitConverter(String fromUnit, String toUnit, double value) {
+		this.fromUnit = fromUnit.toUpperCase();
+		this.toUnit = toUnit.toUpperCase();
+		this.value = value;
+	}
+	
+	public double convert() {
+		  if (((fromUnit.equals("METER")) || fromUnit.equals("METERS"))) {
+			  return fromMeters(value);
+		  }
+		  else if (((toUnit.equals("METER")) || toUnit.equals("METERS"))) {
+			  return toMeters(value);
+		  }
+		  else {
+			  return -1;
+		  }
 	}
 		
-	public double toMeters(double val) 
-	 {
-	  if(fromUnit.equals("in"))
-	  {
-	   meters = (val*INCHES);
+	public double toMeters(double val) {
+	  if (((fromUnit.equals("INCHES")) || fromUnit.equals("INCH"))) {
+		  converted = (val*INCHES); 
 	  }
-	  else if(fromUnit.equals("ft"))
-	  {
-	   meters = (val*FEET);
+	  else if (((fromUnit.equals("FT")) || fromUnit.equals("FEET"))) {
+		  converted = (val*FEET);
 	  }
-	  else if(fromUnit.equals("mi"))
-	  {
-	   meters = (val*MILES);
+	  else if ((fromUnit.equals("MILES") || fromUnit.equals("MILE"))) {
+		  converted = (val*MILES);
 	  }
-	  else if(fromUnit.equals("mm"))
-	  {
-	   meters = (val*MILLIMETERS);
+	  else if ((fromUnit.equals("MM") || fromUnit.equals("MMS"))) {
+		  converted = (val*MILLIMETERS);
 	  }
-	  else if(fromUnit.equals("cm"))
-	  {
-	   meters = (val*CENTIMETERS);
+	  else if ((fromUnit.equals("CM") || fromUnit.equals("CMS"))) {
+		  converted = (val*CENTIMETERS);
 	  }
-	  else if(fromUnit.equals("m"))
-	  {
-	   meters = (val*METERS);
+	  else if ((fromUnit.equals("M") || fromUnit.equals("METER"))) {
+		  converted = (val*METERS);
 	  }
-	  else
-	  {
-	   meters = (val*KILOMETERS);
+	  else if ((fromUnit.equals("KM") || fromUnit.equals("KMS"))) {
+		  converted = (val*KILOMETERS);
 	  }
-	  return meters;
-	 }
-	 
-	 public double fromMeters(double meters) 
-	 {
-	  if(fromUnit.equals("in"))
-	  {
-	   converted = Math.round(meters*39.369923740457715);
-	  }
-	  else if(fromUnit.equals("ft")) 
-	  {
-	   converted = Math.round(meters*3.280839895013123);
-	  }
-	  else if(fromUnit.equals("mi"))
-	  {
-	   converted = Math.round(meters*0.0006213688756330196);
-	  }
-	  else if(fromUnit.equals("mm")) 
-	  {
-	   converted = Math.round(meters*1000);
-	  }
-	  else if(fromUnit.equals("cm")) 
-	  {
-	   converted = Math.round(meters*100);
-	  }
-	  else if(fromUnit.equals("m")) 
-	  {
-	   converted = Math.round(meters*1);;
-	  }
-	  else 
-	  {
-	   converted = Math.round(meters*0.001);
+	  else {
+		  converted = -1;
 	  }
 	  return converted;
+	 }
+	 
+	 public double fromMeters(double meters) {
+		 if (((fromUnit.equals("INCHES")) || fromUnit.equals("INCH"))) {
+			 converted = Math.round(meters*39.369923740457715);
+		 }
+		 else if (((fromUnit.equals("FT")) || fromUnit.equals("FEET"))) {
+			 converted = Math.round(meters*3.280839895013123);
+		 }
+		 else if ((fromUnit.equals("MILES") || fromUnit.equals("MILE"))) {
+			 converted = Math.round(meters*0.0006213688756330196);
+		 }
+		 else if ((fromUnit.equals("MM") || fromUnit.equals("MMS"))) {
+			 converted = Math.round(meters*1000);
+		 }
+		 else if ((fromUnit.equals("CM") || fromUnit.equals("CMS"))) {
+			 converted = Math.round(meters*100);
+		 }
+		 else if ((fromUnit.equals("M") || fromUnit.equals("METER"))) {
+			 converted = Math.round(meters*1);;
+		 }
+		 else if ((fromUnit.equals("KM") || fromUnit.equals("KMS"))) {
+			 converted = Math.round(meters*0.001);
+		 }
+		 else {
+			 converted = -1;
+		 }
+		 return converted;
 	 }	
 }
